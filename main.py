d839c889e9d4f05ce7215e5ca8e2d94311fcc4e1
vadimzhenchur <152019836+Picok228@users.noreply.github.com>
2024-03-16 19:39:11 +0200

    foto_shop0.2

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
+import os
+from PIL.ImageFilter import *
+from PyQt5.QtGui import QPixmap, QImage
+from PIL import Image
+
 
 
 app = QApplication([])
@@ -36,6 +41,50 @@ knopki.addWidget(v_pravo)
 knopki.addWidget(dzerkalo)
 knopki.addWidget(rizkist)
 knopki.addWidget(HB)
+
+class Bubliki:
+    def __int__(self):
+        self.image = None
+        self.folder = None
+        self.image_name = None
+    def load(self):
+        full_path = os.path.join(self.folder, self.image_name)
+        self.image = Image.open(full_path)
+    def show_imege(self):
+        pixel = pil2pixmap(self.image)
+        kartinka.setPixmap(pixel)
+
+bubliki = Bubliki()
+
+def show_sistem():
+    bubliki.folder =QFileDialog.getExistingDirectory()
+    list_files = os.listdir(bubliki.folder)
+    pole.clear()
+    for file in list_files:
+        #якщо цей файл
+        fileList.addItems(file)
+    pole.addItems(list_files)
+
+
+def show_photo():
+    image_name = pole.currentItem().text()
+    bubliki.image_name = image_name
+    bubliki.load()
+    bubliki.show_imege()
+def pil2pixmap(im):
+    if im.mode == "RGB":
+        r, g, b = im.split()
+        im = Image.merge("RGB", (b, g, r))
+    elif im.mode == "RGBA":
+        r, g, b, a = im.split()
+        im = Image.merge("RGBA", (b, g, r, a))
+    elif im.mode == "L":
+        im = im.convert("RGBA")
+    im2 = im.convert("RGBA")
+    data = im2.tobytes("raw", "RGBA")
+    qim = QImage(data, im.size[0], im.size[1], QImage.Format_ARGB32)
+    pixmap = QPixmap.fromImage(qim)
+    return pixmap
 
 app.setStyleSheet("""
     QWidget
@@ -85,10 +134,10 @@ app.setStyleSheet("""
         border-style: double;      
     }
 
-
-
 """)
 
-
+pole.currentRowChanged.connect(show_photo)
+papka.clicked.connect(show_sistem)
 window.setLayout(Golovna_linia)
+window.show()
 app.exec_()
